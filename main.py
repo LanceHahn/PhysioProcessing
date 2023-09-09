fa6bcad6f8490835ac35187758e0a1571526dbb1
Addie Patterson <addie.patterson751@topper.wku.edu>
2023-09-09 16:18:02 -0500

    Reads data file to display sample rate, number of samples, highs and lows in amplitude, and when these minimums and maximums occur.

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 
 from scipy.io import wavfile
-samplerate, data = wavfile.read('./output/audio.wav')
-print("testing main")
+import math
+
+samplerate, data = wavfile.read('BYB_Recording_2023-09-08_11_35_48.wav') # read data file to find num of samples per a second and the amplitude at each sample
+length = data.shape[0] / samplerate; # calculate the length of the audio file in seconds
+print(f"{length}s")
+print(f"Sample Rate = {samplerate}Hz") # number of samples per a second
+print(f"Number of Samples = {data.shape[0]}") # number of samples in data set
+maxAmplitude = max(data) # find the amplitude at the highest point
+minAmplitude = min(data) # find the amplitude at the lowest point
+print(f"Highest Amplitude = {maxAmplitude}")
+print(f"Lowest Amplitude = {minAmplitude}")
+for i in range(data.shape[0]):
+    if maxAmplitude == data[i]: # compare each sample's amplitude to test if it is the maximum
+        print(f"max {i / samplerate}s") # display when at maximum point
+    elif minAmplitude == data[i]: # compare each sample's amplitude to test if it is the minimum
+        print(f"min {i / samplerate}s") # display when at minimum point
+
