4f9338d30f9a7e07da555c833bc0cd51ab2a0d30
Lance Hahn <41932220+LanceHahn@users.noreply.github.com>
2024-04-08 16:19:51 -0500

    modularize, find multiple waves

--- blinkDection.py
+++ blinkDection.py
@@ -4,7 +4,86 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 
-def findBlinks(vData, blinkDuration, interBlink, sampleHz=1000):
+def plotMotifDiscovery(timeLabels, vData, distData, targetIX, matchIX, wwidth):
+
+    # Plot the original wave form with colorful patches highlighting the detected pattern
+    # and the matrix profile distance values with the lowest distance pair highlighted
+    vMax = max(vData)
+    vMin = min(vData)
+    height = vMax - vMin
+    fig, axs = plt.subplots(2, sharex=True, gridspec_kw={'hspace': 0})
+    plt.suptitle('Motif (Pattern) Discovery', fontsize='14')
+    axs[0].plot(timeLabels, vData)
+    axs[0].set_ylabel('EEG', fontsize='14')
+    rect0 = Rectangle((timeLabels[targetIX], vMin), wwidth, height=height, facecolor='lightgrey')
+    axs[0].add_patch(rect0)
+    rect1 = Rectangle((timeLabels[matchIX], vMin), wwidth, height=height, facecolor='orange')
+    axs[0].add_patch(rect1)
+    axs[1].set_xlabel('Time', fontsize='14')
+    axs[1].set_ylabel('Matrix Profile', fontsize='14')
+    axs[1].axvline(x=timeLabels[targetIX], linestyle="dashed")
+    axs[1].axvline(x=timeLabels[matchIX], linestyle="dotted")
+    axs[1].tick_params(labelbottom=True)
+    axs[1].plot(timeLabels[:len(distData)], distData)
+    plt.show()
+
+    return
+
+def plotMotifMatch(vData, targetIX, matchIX, wwidth, dist):
+    # plot a window that includes the two matched patterns with 2x window size border
+    # and the two matched waveforms on top of one another
+    vMax = max(vData)
+    vMin = min(vData)
+    height = vMax - vMin
+    fig, axs = plt.subplots(2)
+    plt.suptitle(f'Motix Match (dist: {dist})', fontsize='14')
+    sMin = max(0, min(targetIX - (2*wwidth), matchIX - (2*wwidth)))
+    sMax = min(len(vData), max(targetIX + (3*wwidth), matchIX + (3*wwidth)))
+    axs[0].plot(vData[sMin:sMax])
+    axs[0].set_ylabel('EEG', fontsize='14')
+    rect0 = Rectangle((targetIX-sMin, vMin), wwidth, height=height,
+                      facecolor='palegreen')
+    axs[0].add_patch(rect0)
+    rect1 = Rectangle((matchIX-sMin, vMin), wwidth, height=height,
+                      facecolor='orange')
+    axs[0].tick_params(labelbottom=False)
+    axs[0].add_patch(rect1)
+
+    axs[1].set_xlabel("Time", fontsize='20')
+    axs[1].set_ylabel("Motif", fontsize='20')
+    axs[1].plot(vData[targetIX:targetIX + wwidth], color='palegreen')
+    axs[1].plot(vData[matchIX:matchIX + wwidth], color='orange')
+    plt.show()
+    return
+
+
+def plotWaves(waves, xLabels=[], labels=[], zNorm=True, title="Wave Plot"):
+    # plot a window that includes the two matched patterns with 2x window size border
+    # and the two matched waveforms on top of one another
+    for ix, waveO in enumerate(waves):
+        if zNorm:
+            wave = stumpy.core.z_norm(waveO)
+        else:
+            wave = waveO
+        if xLabels:
+            plt.plot(xLabels, wave,
+                     label=labels[ix])
+        else:
+            plt.plot(wave,
+                     label=labels[ix])
+    plt.title(title)
+    plt.legend()
+    plt.show()
+    return
+
+def combineWaves(waves, weights=[]):
+    if weights == []:
+        weights = [1/len(waves)]* len(waves)
+    waveProduct = np.sum([waves[ix] * weights[ix] for ix in range(len(waves))], axis=0)
+    return waveProduct
+
+def findBlinkWave(vData, blinkDuration, interBlink, sampleHz=1000, tLabels=[],
+                  verbose=10):
     """
     Return a list of tuples which define the start and end time of a blink
     in seconds
@@ -14,62 +93,83 @@ def findBlinks(vData, blinkDuration, interBlink, sampleHz=1000):
     :param sampleHz: the number of samples per second in the data provided
     :return: [(blink_start_seconds, blink_end_seconds), (), ...]
     """
-    blinks = []
-    vMax = max(vData)
-    vMin = min(vData)
-    height = vMax - vMin
     window_size = int(blinkDuration * sampleHz)  #  data points found in a pattern
-    print(f"Looking across {len(vData)/sampleHz}s sampled at {sampleHz}Hz ({len(vData)} points) with a window of {blinkDuration}s ({window_size} points)")
+    if verbose > 2:
+        print(f"Looking across {len(vData)/sampleHz}s sampled at {sampleHz}Hz "
+              f"({len(vData)} points) with a window of {blinkDuration}s ({window_size} points)")
     matrix_profile = stumpy.stump(vData, m=window_size)
     mp = matrix_profile
     motif_idx = np.argsort(mp[:, 0])[0]
-    print(f"The motif is located at index {motif_idx}")
-
+    if verbose > 2:
+        print(f"The motif is located at index {motif_idx}")
     nearest_neighbor_idx = mp[motif_idx, 1]
-    print(f"The nearest neighbor is located at index {nearest_neighbor_idx}")
+    closestDistance = mp[motif_idx][0]
 
-    fig, axs = plt.subplots(2, sharex=True, gridspec_kw={'hspace': 0})
+    if verbose > 2:
+        print(f"The nearest neighbor is located at index {nearest_neighbor_idx}")
+    if verbose > 8:
+        plotMotifDiscovery(tLabels, vData, mp[:, 0],
+                           motif_idx, nearest_neighbor_idx,
+                           window_size/sampleHz)
+    if verbose > 7:
+        plotMotifMatch(vData, motif_idx, nearest_neighbor_idx, window_size,
+                       closestDistance)
 
-    # Plot the original wave form with colorful patches highlighting the detected pattern
-    # and the matrix profile distance values with the lowest distance pair highlighted
-    plt.suptitle('Motif (Pattern) Discovery', fontsize='14')
-    axs[0].plot(vData)
-    axs[0].set_ylabel('EEG', fontsize='14')
-    rect0 = Rectangle((motif_idx, vMin), window_size, height=height, facecolor='lightgrey')
-    axs[0].add_patch(rect0)
-    rect1 = Rectangle((nearest_neighbor_idx, vMin), window_size, height=height, facecolor='orange')
-    axs[0].add_patch(rect1)
-    axs[1].set_xlabel('Time', fontsize='14')
-    axs[1].set_ylabel('Matrix Profile', fontsize='14')
-    axs[1].axvline(x=motif_idx, linestyle="dashed")
-    axs[1].axvline(x=nearest_neighbor_idx, linestyle="dotted")
-    axs[1].plot(mp[:, 0])
-    plt.show()
+    blinkWave = combineWaves([vData[motif_idx:motif_idx + window_size],
+                             vData[nearest_neighbor_idx:nearest_neighbor_idx + window_size]],
+                             [0.5, 0.5])
+    if verbose > 6:
+        plotWaves([vData[motif_idx:motif_idx + window_size],
+                    vData[nearest_neighbor_idx:nearest_neighbor_idx + window_size],
+                    blinkWave],
+                  xLabels=[],
+                  labels=[tLabels[motif_idx], tLabels[nearest_neighbor_idx], "Blink"])
 
-    # plot a window that includes the two matched patterns with 2x window size border
-    # and the two matched waveforms on top of one another
-    fig, axs = plt.subplots(2)
-    plt.suptitle('Motif (Pattern) Discovery', fontsize='14')
-    sMin = max(0, min(motif_idx - (2*window_size), nearest_neighbor_idx - (2*window_size)))
-    sMax = min(len(vData), max(motif_idx + (3*window_size), nearest_neighbor_idx + (3*window_size)))
-    axs[0].plot(vData[sMin:sMax])
-    axs[0].set_ylabel('EEG', fontsize='14')
-    rect0 = Rectangle((motif_idx-sMin, vMin), window_size, height=height, facecolor='palegreen')
-    axs[0].add_patch(rect0)
-    rect1 = Rectangle((nearest_neighbor_idx-sMin, vMin), window_size, height=height, facecolor='orange')
-    axs[0].add_patch(rect1)
+    return blinkWave
 
-    axs[1].set_xlabel("Time", fontsize='20')
-    axs[1].set_ylabel("Motif", fontsize='20')
-    axs[1].plot(vData[motif_idx:motif_idx + window_size], color='palegreen')
-    axs[1].plot(vData[nearest_neighbor_idx:nearest_neighbor_idx + window_size], color='orange')
-    plt.show()
+def findBlinks(initWave, vData, blinkDuration, sampleHz=1000,
+                      tLabels=[], verbose=10):
+    """
+    Return a list of tuples which define the start and end time of a blink
+    in seconds
+    :param vData: time series data
+    :param blinkDuration: expected blink duration in seconds
+    :param sampleHz: the number of samples per second in the data provided
+    :return: [(blink_start_seconds, confidence), (), ...]
+    """
+    window_size = int(blinkDuration * sampleHz)  # data points found in a pattern
+    if verbose > 2:
+        print(f"Looking across {len(vData) / sampleHz}s sampled at {sampleHz}Hz ({len(vData)} points) with a window of {blinkDuration}s ({window_size} points)")
+    distance_profile = stumpy.mass(initWave, vData)
+    if verbose > 8:
+        plt.plot(tLabels[:len(distance_profile)],
+                 distance_profile,
+                 label="Dissimilarity from target wave")
+        plt.title('Distance Profile')
+        plt.show()
 
-    print(f"z-normalized Euclidean between the two waveforms: {mp[motif_idx, 0]}")
-    eucSort = np.sort(mp[:, 0])
-    plt.plot(eucSort)
-    plt.title('Euclidean distances sorted')
-    plt.legend()
-    plt.show()
+    idx = np.argmin(distance_profile)
+    if verbose > 2:
+        print(f"The best match to Blink Template is located at index {idx} "
+              f"(time: {tLabels[idx]})")
+    disProf = np.argsort(distance_profile)
+    disThresh = 2
+    candidateCount = np.argsort(np.where(distance_profile < disThresh)).size
+    blinkIxs = [idx]  # blink start times
+    wwidth = len(initWave)
+    for ix in disProf[:candidateCount]:
+        if any(b - wwidth < ix < b + wwidth for b in blinkIxs):
+            pass
+        else:
+            if verbose > 3:
+                print(f"Adding {ix} {tLabels[ix]} {distance_profile[ix]}")
+            blinkIxs.append(ix)
+    blinks = [tLabels[ix] for ix in blinkIxs]
+    if verbose > 3:
+        print(f"{len(blinks)} blinks found at {blinks}")
 
+    if verbose > 6:
+        plotWaves([initWave]+[vData[bix:bix + window_size] for bix in blinkIxs],
+                  xLabels=[],
+                  labels=["Blink"] + blinks, title=f"Waves Found ({len(blinkIxs)})")
     return blinks
--- plotElectrodeResponses.py
+++ plotElectrodeResponses.py
@@ -2,7 +2,8 @@ import os
 import pwd
 from mne.io.eeglab import read_raw_eeglab, read_epochs_eeglab
 import matplotlib.pyplot as plt
-from blinkDection import findBlinks
+import numpy as np
+from blinkDection import findBlinkWave, findBlinks, combineWaves, plotWaves
 def get_username():
     return pwd.getpwuid(os.getuid())[0]
 
@@ -32,14 +33,22 @@ maxReal = 0.01
 sampleRate = 1000
 # goodChannels = [x for x in electLabels if x[-1] == '1']
 # goodChannels = ["E14", "E21"]
-channelString = input("Provide a space-delimited list of channel numbers: ")
+channelString = input("Provide a space-delimited list of channel numbers (defaulty 14): ")
+if len(channelString) < 1:
+    channelString = '14'
 goodChannels = channelString.split(' ')
 goodChannels = ["E" + ch for ch in goodChannels]
 startTime = int(60 * 21)
 endTime = int(60 * 22)
 sampleRate = 1000
-startTime = int(input("Provide a start time in seconds:"))
-endTime = int(input("Provide a end time in seconds:"))
+timeString = input("Provide a start time in seconds (default: 707):")
+if len(timeString) < 1:
+    timeString = '707'
+startTime = int(timeString)
+timeString = input("Provide a end time in seconds (default: 721):")
+if len(timeString) < 1:
+    timeString = '721'
+endTime = int(timeString)
 print("Generating plots..")
 
 for electIX in range(len(electLabels)):
@@ -51,14 +60,17 @@ for electIX in range(len(electLabels)):
         adjusted = [x - maxV + base if x < maxReal else None for x in allData[electIX]]
         #adjusted = [x for x in allData[electIX]]
         #plt.plot(tLabels, adjusted, label=electLabels[electIX])
-        plt.plot(tLabels[startTime*1000:endTime*1000], adjusted[startTime*1000:endTime*1000], label=electLabels[electIX])
+        plt.plot(tLabels[startTime*1000:endTime*1000],
+                 adjusted[startTime*1000:endTime*1000],
+                 label=electLabels[electIX])
         base += -0.0003 + minV -maxV # offset
 
 plt.title('initial EEG plot')
 plt.legend()
 plt.show()
 
-blinkDuration = 150 / sampleRate  #150 ms #0.3 # 0.1  # unit: seconds
+blinkDurationMS = 150
+blinkDuration = blinkDurationMS / sampleRate  #150 ms #0.3 # 0.1  # unit: seconds
 interBlink = 60 / 12  # 12 times per minute unit: seconds
 dwnSample = 1
 for electIX in [x for x in range(len(electLabels)) if electLabels[x] in goodChannels]:
@@ -66,7 +78,30 @@ for electIX in [x for x in range(len(electLabels)) if electLabels[x] in goodChan
         sequ = [allData[electIX][x] for x in range(startTime*sampleRate, endTime*sampleRate, dwnSample)]
     else:
         sequ = allData[electIX][startTime * sampleRate:endTime * sampleRate]
-    blinks = findBlinks(sequ, blinkDuration, interBlink, sampleHz=int(sampleRate/dwnSample))
+    # Find initial blink wave as best duplicated sequence.
+    blinkWave = findBlinkWave(sequ, blinkDuration, interBlink,
+                              sampleHz=int(sampleRate/dwnSample),
+                              tLabels=tLabels[startTime*1000:endTime*1000],
+                              verbose=7)
 
+    blinks = findBlinks(blinkWave, sequ, blinkDuration,
+                        sampleHz=int(sampleRate/dwnSample),
+                        tLabels=tLabels[startTime*1000:endTime*1000],
+                        verbose=7)
+    startIndecies = [np.where(tLabels == b)[0][0] - (startTime*1000) for b in blinks]
+    newBlinkWave = combineWaves([sequ[start: start + blinkDurationMS]
+                                 for start in startIndecies])
+    plotWaves([blinkWave, newBlinkWave], xLabels=[],
+              labels=["Blink", "NewBlink"])
+    print(f"Blinks per minute: {len(blinks)/((endTime-startTime)/60)}")
+
+    # Try again with new updated wave
+    print(f"Repeat blink discover with wave generated from {len(blinks)} "
+          f"detected waves.")
+    blinks2 = findBlinks(newBlinkWave, sequ, blinkDuration,
+                        sampleHz=int(sampleRate/dwnSample),
+                        tLabels=tLabels[startTime*1000:endTime*1000],
+                        verbose=7)
+    print(f"Blinks per minute: {len(blinks2)/((endTime-startTime)/60)}")
 akey = input('press return when ready to quit')
 print("done")
